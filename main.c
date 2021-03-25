a4524b2202f9ed9d836e0df56c4edcbbabb290f9
Pedro Cardoso <phlc.cardoso@gmail.com>
2021-03-24 22:23:37 -0300

    Item B
    To-do condicionais de validação

--- main.c
+++ main.c
@@ -10,28 +10,83 @@ typedef struct Pessoa{
     int tipo_conta;
     float limite;
     int data_vencimento;
+    float saldo;
 } Pessoa;
 
 void clean_stdin();
-void cadastrar(Pessoa cliente[]);
+int cadastrar(Pessoa cliente[],int i);
+void criaMovimento(int data);
 
 int main(){
     Pessoa cliente[CONTAS];
-    
-    cadastrar(cliente);
+    int index, n;
+    int data;
 
+    index = cadastrar(cliente,0);
+    system("cls");
 
+    do {
+        //MENU
+        printf("1. Cadastrar mais clientes\n");
+        printf("2. Gerar arquivo de movimentacao a partir de data\n");
+        printf("3. Atualizar cadastro a partir de 'movimento'\n");
+        printf("4. Lista de depositos realizados a partir do numero da conta\n");
+        printf("5. Consultar saldo a partir do numero da conta\n");
+        printf("6. Sair\n\n");
+        printf("Digite a opcao desejada: ");
+        scanf("%d", &n);
 
+        switch(n){
+            case 1:
+                system("cls");
+                cadastrar(cliente, index);
+            break;
+            case 2:
+                system("cls");
+                printf("Insira a data para criar o arquivo Movimento: ");
+                scanf("%d", &data);
+                printf("\n\n");
+                criaMovimento(data);
+            break;
+            case 3:
+                system("cls");
+                
+            break;
+            case 4:
+                system("cls");
+                
+            break;
+            case 5:
+                system("cls");
+                
+            break;
+            case 6:
+                exit(0);
+            break;
+            default:
+                system("cls");
+                printf("A opcao informada: %d e invalida\n", n);
+        }
+        
+        printf("\n\nPressione qualquer tecla para continuar...");
+        clean_stdin();
+        getchar();
+    } while(n != 6);
 
     return 0;
 }
 
-void cadastrar(Pessoa cliente[]){
+void clean_stdin(){
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+int cadastrar(Pessoa cliente[], int i){
     FILE *arq;
     FILE *arq2;
-    int i = 0, status;
+    int status;
     char c;
-
+    
     arq = fopen("./arquivosBin/cadastros.bin", "wb");
     if(arq == NULL){
         perror("Erro ao abrir o arquivo");
@@ -43,9 +98,10 @@ void cadastrar(Pessoa cliente[]){
         exit(1);
     }
 
-    while(c!=27){
+    while(1){
         printf("Digite as informacoes do cliente:\n");
         cliente[i].numero_conta = i+1;
+        cliente[i].saldo = 0;
         printf("Nome do cliente: ");
         scanf("%[^\n]", cliente[i].nome_cliente);
         printf("Data de Abertura: ");
@@ -78,9 +134,11 @@ void cadastrar(Pessoa cliente[]){
         fprintf(arq2, "Data de Vencimento: %d\n", cliente[i].data_vencimento);
         puts("Arquivo .txt gravado com sucesso!");
         
-        printf("\n\nPressione qualquer tecla para cadastrar o próximo cliente ou ESC para sair do cadastro...");
+        printf("\n\nPressione qualquer tecla para cadastrar o próximo cliente ou insira 9 para sair do cadastro...");
         clean_stdin();
         c = getchar();
+        if(c == 57)
+            break;
         clean_stdin();
         
         /*
@@ -98,9 +156,55 @@ void cadastrar(Pessoa cliente[]){
 
     fclose(arq2);
     fclose(arq);
+    return i;
 }
 
-void clean_stdin(){
-    int c;
-    while((c = getchar()) != '\n' && c != EOF);
+void criaMovimento(int data){
+    FILE *arq, *arq2;
+    int nConta, tipoOp, j;
+    int status;
+    char c;
+    float valor;
+
+    arq = fopen("./arquivosBin/movimento.bin", "wb");
+    if(arq == NULL){
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+    arq2 = fopen("./arquivosTxt/movimento.txt", "w");
+    if(arq2 == NULL){
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+        
+        fprintf(arq, "%d\n", data);
+        fprintf(arq2, "%d\n", data);
+    while(1){
+        printf("Digite a conta do cliente: ");
+        scanf("%d", &nConta);
+        printf("Digite o tipo da operacao (1 - depósito, 2 - saque): ");
+        scanf("%d", &tipoOp);
+        printf("Digite o valor: ");
+        scanf("%f", &valor);
+
+        //Gravando arquivo .bin
+        fprintf(arq, "%d; %d; %.2f\n", nConta, tipoOp, valor);
+        
+        //Gravando arquivo .txt
+        fprintf(arq2, "%d; %d; %.2f\n", nConta, tipoOp, valor);
+        
+        printf("\n\nPressione qualquer tecla para inserir a proxima movimentacao ou insira 9 para sair ...");
+        clean_stdin();
+        c = getchar();
+        if(c == 57)
+            break;
+        clean_stdin();
+
+        j++;
+    }
+    puts("Arquivo .bin gravado com sucesso!");
+    puts("Arquivo .txt gravado com sucesso!");
+
+    fclose(arq);
+    fclose(arq2);
 }
