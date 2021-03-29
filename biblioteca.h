53618f5f7a41f9bbb793b5af7af0ab865d0dd778
Pedro Cardoso <phlc.cardoso@gmail.com>
2021-03-29 05:43:11 -0300

    Big update
    Faltando apenas a procedure de consulta e refinamentos

---
+++ biblioteca.h
@@ -0,0 +1,15 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+#include "biblioteca.c"
+
+typedef struct Movimento;                     //Declaração da Struct do tipo Movimento
+typedef struct Pessoa;                        //Declaração da Struct do tipo Pessoa
+void clean_stdin();                           //Procedure para limpar o buffer do teclado
+int cadastrarClientes(Pessoa cliente, int i); //Função para cadastrar novos clientes
+void appendMovimento();                       //Procedure para gravar dados no arquivo "Movimento"
+void createFiles();                           //Procedure para criar os arquivos "Cadastro" e "Movimento" no formato .txt e .bin
+void relatorioMovimento();                    //Gera um relatório de movimentações a partir de uma data
+int updateCadastro(int indexUpdate);          //Atualiza o saldo do cadastro a partir de um arquivo "Movimento"
+void listDeposito();                          //Lista todos os depositos feitos na conta de determinado cliente
+
+#endif
